f40cd1b1ffcc86a7c1d8c653c6c8b0e0b37a04dd
bolongz <blzhang.m@gmail.com>
2020-02-10 16:59:38 -0500

    updates

--- RandomLU/RandLU_b.m
+++ RandomLU/RandLU_b.m
@@ -3,6 +3,7 @@ function [ L, U] = RandLU_b(A,k, b, p)
 [m,n]=size(A);
 L = [];
 U = [];
+Lp = [];
 i = 0;
 while i <= k
     Omg = randn(n, b);
@@ -11,9 +12,20 @@ while i <= k
         [Li, ~] = lu(A' * Li);
         [Li, ~] = lu(A * Li);
     end
+    if k > 0
+        [Li, ~] = lu(Li - L * (Lp * Li));
+    end
+    Lip = Fastpinv(Li, 'gauss');
+    Ui = Lip * A;
+    L = [L, Li];
+    U = [U;Ui];
+    Lp = [Lp; Lip];
+    
+    %{
     Ui = Fastpinv(Li, 'gauss') * A;
     L = [L, Li];
     U = [U;Ui];
+    %}
     A = A - Li * Ui;
     i = i + b;
     E = norm(A, 'fro')^2;
--- RandomLU/test_script2.m
+++ RandomLU/test_script2.m
@@ -2,7 +2,7 @@
 sp1_randlub;
 sp2_randlub;
 sp3_randlub;
-Acc_randlub1.m
-Acc_randlub2.m
-Acc_randlub3.m
+Acc_randlub1;
+Acc_randlub2;
+Acc_randlub3;
 exit;
